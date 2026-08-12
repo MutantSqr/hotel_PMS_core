7e548603560702e290ab3cbd3cd7eef7f7c39c3d
Bowser <55066833+MutantSqr@users.noreply.github.com>
2026-08-11 23:12:17 -0500

    Expose room inventory operations on HotelAssistant

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,13 @@
 from app.assist import HotelAssistant
 from app.room_moves import change_room
+from app.room_status import set_room_status, mark_room_out_of_order, mark_room_out_of_service, restore_room
+from app.room_inventory import set_showroom, inventory_by_type, room_is_sellable
 
 HotelAssistant.change_room = change_room
+HotelAssistant.set_room_status = set_room_status
+HotelAssistant.mark_room_out_of_order = mark_room_out_of_order
+HotelAssistant.mark_room_out_of_service = mark_room_out_of_service
+HotelAssistant.restore_room = restore_room
+HotelAssistant.set_showroom = set_showroom
+HotelAssistant.inventory_by_type = inventory_by_type
+HotelAssistant.room_is_sellable = room_is_sellable
