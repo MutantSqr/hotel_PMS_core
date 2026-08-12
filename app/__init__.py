1f3bf2a930826f2d8c0ee6524aca0e42bebd8e1f
Bowser <55066833+MutantSqr@users.noreply.github.com>
2026-08-11 22:40:26 -0500

    Expose room status state machine through HotelAssistant

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,9 @@
 from app.assist import HotelAssistant
 from app.room_moves import change_room
+from app.room_status import set_room_status, mark_room_out_of_order, mark_room_out_of_service, restore_room
 
 HotelAssistant.change_room = change_room
+HotelAssistant.set_room_status = set_room_status
+HotelAssistant.mark_room_out_of_order = mark_room_out_of_order
+HotelAssistant.mark_room_out_of_service = mark_room_out_of_service
+HotelAssistant.restore_room = restore_room
